72bfb178815da98287da3c81b87c5fcf48b45d3c
dymnz <vicodin1123@gmail.com>
2017-05-06 22:05:47 +0800

    ye

--- hw2/ecg_hfn.m
+++ hw2/ecg_hfn.m
@@ -14,6 +14,35 @@ xlabel('Time in seconds');
 ylabel('ECG');
 title('Original signal');
 
-% Filter: Order 2, cutoff 10Hz
-result = butter_filter(ecg, 2, 10, fs);
-figure; plot(t, result);
+% Filters
+orders = [2 8 8 8];
+FCs = [10 20 40 70];
+N = length(orders);
+
+figSignal = figure;
+figFreq = figure;
+% Filtering
+for i = 1 : N
+    order = orders(i); fc = FCs(i);
+    [result, b, a] = butter_filter(ecg, order, fc, fs);
+    
+    % Plot filtered signal
+    figure(figSignal);
+    subplot(N,1,i); plot(t, result);
+    title(sprintf('Order: %d, fc = %dHz', order, fc))
+    xlabel('Time (sec)')
+    ax = gca;
+    ax.YLim = [-3 3];
+    
+    % Plot frequency response
+    figure(figFreq);
+    subplot(N,1,i);     
+    [h, w]= freqz(b, a, fs);
+    plot(w/pi*fs, 20*log10(abs(h)));
+    title(sprintf('Order: %d, fc = %dHz', order, fc))
+    xlabel('Frequency (Hz)')
+    ylabel('Magnitude (dB)')
+    ax = gca;
+    ax.YLim = [-100 20];
+    ax.XLim = [0 fs];    
+end
